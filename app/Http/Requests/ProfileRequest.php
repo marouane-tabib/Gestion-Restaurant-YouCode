89f528ef5d5e423af7bcb4579a6f3c93437e3560
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-31 17:28:20 +0100

    add add ProfileRequest - workout

---
+++ app/Http/Requests/ProfileRequest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        switch ($this->method()){
+            case 'POST' : {
+                return [
+                    'name' => ['required', 'string', 'max:255'],
+                    'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+                    'password' => ['required', 'string', 'min:8', 'confirmed'],
+                ];
+            }
+            case 'PUT' : {
+                //
+            }
+            case 'PATCH' : {
+                return [
+                    'name' => ['required', 'string', 'max:255'],
+                    'email' => ['required', 'string', 'email', 'max:255', 'unique:users,id'.$this->route()->profile()->id],
+                    'password' => ['required', 'string', 'min:8', 'confirmed'],
+                ];
+            }
+            default: break;
+        }
+        return [
+            //
+        ];
+    }
+}
