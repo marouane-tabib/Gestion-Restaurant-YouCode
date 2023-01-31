db14743bbbc2ab6bbbed5b0ff63a5de68aa06d35
marwan-tabib <marwanmaroc297@gmail.com>
2023-01-31 11:54:47 +0100

    GRY-4 create profile view

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/profile' , [ProfileController::class , 'index'])->middleware('auth')->name('profile');
 
 Auth::routes();
 
