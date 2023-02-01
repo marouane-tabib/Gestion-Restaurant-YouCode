92cc761a3905aea4d8a7037b51de9f664ddd05e9
marwan-tabib <marwanmaroc297@gmail.com>
2023-02-01 15:03:53 +0100

    add update user information - completed

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Http\Requests\ProfileRequest;
 use App\Models\User;
-use Symfony\Component\HttpKernel\Profiler\Profile;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class ProfileController extends Controller
 {
@@ -13,26 +14,12 @@ class ProfileController extends Controller
         return view('profile');
     }
 
-    // public function update(User $user , ProfileRequest $request){
-    //     dd('Stop here');
-    //     // $user->update($request->validated());
-    //     // $this->authorize('update', $user);
-    //     $this->authorize('update' , $request);
-    //     // $user->update([
-    //     //     'name' => $request->name,
-    //     //     'email' => $request->email,
-    //     //     'updated_at' => now()
-    //     // ]);
-    //     return 'update method runing...';
-    // }
-    public function update(User $user, ProfileRequest $request)
+    public function update(User $user , ProfileRequest $request)
     {
         $user->update([
-            'name' => $request->name,
             'email' => $request->email,
-            'updated_at' => now()
+            'password' => Hash::make($request->password),
         ]);
-
-        return $this->success('profile','Profile updated successfully!');
+        return dump($request->email);
     }
 }
