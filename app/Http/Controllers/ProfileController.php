959e435635cdbe5b5eec0f64f9d683df222eb418
marwan-tabib <marwanmaroc297@gmail.com>
2023-02-01 11:51:01 +0100

    GRY-4 update user information - workout

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,13 +3,36 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ProfileRequest;
+use App\Models\User;
+use Symfony\Component\HttpKernel\Profiler\Profile;
 
 class ProfileController extends Controller
 {
     public function index(){
         return view('profile');
     }
-    public function update(){
-        return 'update method runing...';
+
+    // public function update(User $user , ProfileRequest $request){
+    //     dd('Stop here');
+    //     // $user->update($request->validated());
+    //     // $this->authorize('update', $user);
+    //     $this->authorize('update' , $request);
+    //     // $user->update([
+    //     //     'name' => $request->name,
+    //     //     'email' => $request->email,
+    //     //     'updated_at' => now()
+    //     // ]);
+    //     return 'update method runing...';
+    // }
+    public function update(User $user, ProfileRequest $request)
+    {
+        $user->update([
+            'name' => $request->name,
+            'email' => $request->email,
+            'updated_at' => now()
+        ]);
+
+        return $this->success('profile','Profile updated successfully!');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('/profile' , [ProfileController::class , 'index'])->middleware('auth')->name('profile');
-Route::post('/profile' , [ProfileController::class , 'update'])->middleware('auth')->name('profile.update');
+Route::post('/profile/{user}' , [ProfileController::class , 'update'])->middleware('auth')->name('profile.update');
+
+// Route::middleware(['auth'])->group(function () {
+//     Route::get('profile',[ProfileController::class,'index'])->name('profile');
+//     Route::post('profile/{user}',[ProfileController::class,'update'])->name('profile.update');
+//   });
 
 Auth::routes();
 
