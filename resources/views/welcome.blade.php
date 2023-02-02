ae265f875c8e5ab493b199c6315d77bde038ca6f
marwan-tabib <marwanmaroc297@gmail.com>
2023-02-02 16:53:23 +0100

    GRY-5 add landing page design - completed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,16 +3,21 @@
 @section('content')
 <section class="container">
     <div class="row mx-auto">
-        <div class="col-6 col-md-3 plat-card bg-white px-0 py-2">
-            <div class="col">
-                <img src="https://images.unsplash.com/photo-1519077336050-4ca5cac9d64f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" class="img-fluid" alt="">
-            </div>
-            <div class="col p-2">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                <span class="check float-end">Book Now <i class="fa-solid fa-arrow-right"></i></span>
+        @for ($i = 0; $i < 5; $i++)
+
+        <div class="col-6 col-md-3 p-1">
+            <div class=" plat-card bg-white px-0 pb-2 m-1">
+                <div class="col">
+                    <img src="https://images.pexels.com/photos/14000579/pexels-photo-14000579.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" class="img-fluid" alt="">
+                </div>
+                <div class="col p-2">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                    <span class="check float-end shadow">Book Now <i class="fa-solid fa-arrow-right"></i></span>
+                </div>
             </div>
         </div>
+        @endfor
     </div>
 </section>
 @endsection
