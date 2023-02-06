2e57c141986596fba69c65d76807e24140e25c6f
marwan-tabib <marwanmaroc297@gmail.com>
2023-02-06 10:43:32 +0100

    add static bootstrap table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="container">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard')}} @include('components.plat-modal')</div>
 
@@ -14,7 +14,24 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Handle</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                          </tr>
+                        </tbody>
+                      </table>
                 </div>
             </div>
         </div>
