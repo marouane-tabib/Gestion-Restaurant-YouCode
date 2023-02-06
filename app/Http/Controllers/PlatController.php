60d30c9a1c9195e6c996506f6c4d43cae408164d
marwan-tabib <marwanmaroc297@gmail.com>
2023-02-06 14:59:20 +0100

    GRY-6 add create plat func

--- app/Http/Controllers/PlatController.php
+++ app/Http/Controllers/PlatController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Plat;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PlatController extends Controller
 {
-    //
+    public function create(Plat $plat , Request $request){
+        if($request->file('image')){
+            $file= $request->file('image');
+            $filename= date('YmdHi').$file->getClientOriginalName();
+            $file-> move(public_path('storage/Image'), $filename);
+        }
+        $plat->create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'user_id' => Auth::user()->id ,
+            'image' => $filename
+        ]);
+        return redirect()->back();
+    }
 }
