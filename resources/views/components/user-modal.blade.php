24fdf3ea26babee0c7981cb27960625e5525841b
marwan-tabib <marwanmaroc297@gmail.com>
2023-02-06 10:12:42 +0100

    clean

--- resources/views/components/user-modal.blade.php
+++ resources/views/components/user-modal.blade.php
@@ -12,35 +12,35 @@
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <form method="POST" action="{{ route('profile.update',auth()->id()) }}">
-        <div class="modal-body">
-                @csrf
-                <div class="mb-3">
-                  <label for="email" class="form-label">Your Email</label>
-                  <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" aria-describedby="emailHelp">
+            <div class="modal-body">
+                    @csrf
+                    <div class="mb-3">
+                    <label for="email" class="form-label">Your Email</label>
+                    <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" aria-describedby="emailHelp">
+                    </div>
+                    @error('email')
+                        <span class="text-danger">{{$message}}</span>
+                    @enderror
+                    <div class="mb-3">
+                    <label for="password" class="form-label">Your Password</label>
+                    <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password">
+                    </div>
+                    @error('password')
+                        <span class="text-danger">{{$message}}</span>
+                    @enderror
+                    <div class="mb-3">
+                    <label for="password_confirmation " class="form-label">Confirm Password</label>
+                    <input type="password" class="form-control @error('password_confirmation') is-invalid @enderror" id="password_confirmation " name="password_confirmation">
+                    </div>
+                    @error('password_confirmation')
+                        <span class="text-danger">{{$message}}</span>
+                    @enderror
                 </div>
-                @error('email')
-                    <span class="text-danger">{{$message}}</span>
-                @enderror
-                <div class="mb-3">
-                  <label for="password" class="form-label">Your Password</label>
-                  <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password">
+                <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Save changes</button>
                 </div>
-                @error('password')
-                    <span class="text-danger">{{$message}}</span>
-                @enderror
-                <div class="mb-3">
-                  <label for="password_confirmation " class="form-label">Confirm Password</label>
-                  <input type="password" class="form-control @error('password_confirmation') is-invalid @enderror" id="password_confirmation " name="password_confirmation">
-                </div>
-                @error('password_confirmation')
-                    <span class="text-danger">{{$message}}</span>
-                @enderror
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="submit" class="btn btn-primary">Save changes</button>
-            </div>
-            </form>
+        </form>
       </div>
     </div>
   </div>
