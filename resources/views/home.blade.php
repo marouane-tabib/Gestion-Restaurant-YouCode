cb2a4aa22fe3b4de2771a6e4e0da499b8a3d0474
marwan-tabib <marwanmaroc297@gmail.com>
2023-02-06 10:20:07 +0100

    GRY-6 add plats modale

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header">{{ __('Dashboard')}}
                     <button type="button" class="btn btn-primary float-end" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                      Launch demo modal
+                      Add Plat
                     </button>
                 </div>
 
@@ -33,13 +33,30 @@
             <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        <div class="modal-body">
-            ...
-        </div>
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-        </div>
+        <form method="POST" action="{{ route('profile.update',auth()->id()) }}">
+            <div class="modal-body">
+                @csrf
+                <div class="mb-3">
+                    <label for="title" class="form-label">Your Title</label>
+                    <input type="title" class="form-control @error('title') is-invalid @enderror" id="title" name="title" aria-describedby="titleHelp" placeholder="Add Plat Title">
+                </div>
+                @error('title')
+                    <span class="text-danger">{{$message}}</span>
+                @enderror
+                <div class="mb-3">
+                    <label for="description" class="form-label">Your Title</label>
+                    {{-- <input type="description" class="form-control @error('description') is-invalid @enderror" id="description" name="description" aria-describedby="descriptionHelp"> --}}
+                    <textarea name="description" class="form-control  @error('description') is-invalid @enderror" id="description" cols="30" rows="5"></textarea>
+                </div>
+                @error('description')
+                    <span class="text-danger">{{$message}}</span>
+                @enderror
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Save changes</button>
+            </div>
+        </form>
         </div>
     </div>
     </div>
