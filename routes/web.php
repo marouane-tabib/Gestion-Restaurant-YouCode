564fa11d55f6f2a1932a2cab384d3de9f4d4194e
marwan-tabib <marwanmaroc297@gmail.com>
2023-02-06 15:03:30 +0100

    add plat create in routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PlatController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProfileController;
 
@@ -24,3 +25,4 @@ Route::post('/profile/{user}' , [ProfileController::class , 'update'])->middlewa
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::post('/home', [PlatController::class, 'create'])->name('plat.create');
