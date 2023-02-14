650535d3cd8e7877caf555a6fb41493e820b7e93
marwan-tabib <marwanmaroc297@gmail.com>
2023-02-14 11:31:22 +0100

    add main-table-card class to element table container

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
+    <div class="row justify-content-center main-table-card">
         <div class="container">
             <div class="card">
                 <div class="card-header">{{ __('Dashboard')}} @include('components.plat-modal')</div>
